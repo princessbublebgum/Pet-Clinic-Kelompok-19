93bf33baad4bbb166d720b0146702d882b33c2ed
ZhillanullaelGumelar <zhillan7164@gmail.com>
2024-05-19 00:10:47 +0700

    refactor
    refactor appointment cancelation feature

--- main.py
+++ main.py
@@ -50,3 +50,75 @@ class PetClinicApp(App):
 
 if __name__ == '__main__':
     PetClinicApp().run()
+
+# Buatan Zhill
+import pandas as pd
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+from kivy.uix.spinner import Spinner
+
+# File path to the Excel file
+file = "./Pet-Clinic-Kelompok-19/data_appt.xlsx"
+
+# Load the data from the Excel file
+df_appt = pd.read_excel(file)
+df_appt = df_appt.set_index("Appt")
+db_appt = df_appt.to_dict(orient='list')
+
+# Function to cancel appointment based on name
+def cancel_appt(appt_name):
+    if appt_name in df_appt.index:
+        for key in db_appt:
+            db_appt[key].remove(db_appt[key][df_appt.index.get_loc(appt_name)])
+        df_appt.drop(appt_name, inplace=True)
+        df_appt.reset_index().to_excel(file, index=False)
+        return f"Appointment '{appt_name}' has been cancelled."
+    else:
+        return f"Appointment '{appt_name}' not found."
+
+# Custom widget for the main window
+class CancelApptWindow(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(orientation='vertical', **kwargs)
+        
+        self.label = Label(text="Select Appointment to Cancel:")
+        self.add_widget(self.label)
+        
+        self.spinner = Spinner(
+            text='Select Appointment',
+            values=list(df_appt.index),
+            size_hint=(None, None),
+            size=(1500, 500)
+        )
+        self.add_widget(self.spinner)
+        
+        self.cancel_button = Button(text="Cancel Appointment")
+        self.cancel_button.bind(on_press=self.on_cancel_button_press)
+        self.add_widget(self.cancel_button)
+    
+    def on_cancel_button_press(self, instance):
+        appt_name = self.spinner.text
+        if appt_name != 'Select Appointment':
+            message = cancel_appt(appt_name)
+            self.show_popup(message)
+            self.spinner.text = 'Select Appointment'
+            self.spinner.values = list(df_appt.index)
+    
+    def show_popup(self, message):
+        popup = Popup(title='Notification',
+                      content=Label(text=message),
+                      size_hint=(None, None), size=(100, 700))
+        popup.open()
+
+# Main App class
+class CancelApptApp(App):
+    def build(self):
+        return CancelApptWindow()
+
+# Create and run the app
+if __name__ == '__main__':
+    CancelApptApp().run()
